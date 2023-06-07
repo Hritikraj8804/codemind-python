481f3ab08a25efe6fc8936fe1b789ccdb9590a2c
Hritik Raj <37825309+Hritikraj8804@users.noreply.github.com>
2023-06-07 20:33:44 +0530

    Add files via upload

---
+++ red_1.4(swap).py
@@ -0,0 +1,11 @@
+a,b=map(int,input().split())
+print('---------------')
+print('---------------')
+temp=a
+a=b
+b=temp
+print('a = {}\nb = {}'.format(a,b))
+print('---------------')
+print('---------------')
+
+
